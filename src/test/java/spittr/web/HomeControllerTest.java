f501cf01205856ccfec0afa79512a9d32f9f2499
corly meng <corlymeng@qq.com>
2016-07-12 23:56:06 +0800

    start spring mvc learn

---
+++ src/test/java/spittr/web/HomeControllerTest.java
@@ -0,0 +1,18 @@
+package spittr.web;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+public class HomeControllerTest {
+	@Test
+	public void testHomePage() throws Exception{
+		HomeController homeController = new HomeController();
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+		mockMvc.perform(get("/")).andExpect(view().name("home"));
+	}
+}
